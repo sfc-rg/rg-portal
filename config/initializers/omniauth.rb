a77ccabcf7fa59013b0d201889ee8d3881409ec3
Atsuki Demizu <demmys.atsuki@gmail.com>
2015-09-02 00:42:21 +0900

    wrap oauth configuration object with HashWithIndifferentAccess

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-OAUTH_CONFIG = YAML.load(File.open(Rails.root.join('config', 'oauth.yml')))
+OAUTH_CONFIG = HashWithIndifferentAccess.new YAML.load(File.open(Rails.root.join('config', 'oauth.yml')))
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :slack, OAUTH_CONFIG[:slack][:client_id], OAUTH_CONFIG[:slack][:client_secret], scope: :client, team: OAUTH_CONFIG[:slack][:team_id]
