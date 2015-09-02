c52867df0698909c604269e8d7acc7e4297c9167
Atsuki Demizu <demmys.atsuki@gmail.com>
2015-09-02 14:56:21 +0900

    Separate class information from TopPage

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -5,11 +5,24 @@ module PagesHelper
     page.render_content
   end
 
+  def include_or_create_page_content(path, message)
+    include_page_content(
+      path,
+      "<div><a href='#{edit_page_path(path)}'>#{message}</a></div>"
+    )
+  end
+
   def page_breadcrumb_links(page)
     paths = page.path.split('/')
     links = paths.map.with_index do |path, index|
       link = link_to(path, page_path(paths.take(index + 1).join('/')))
       "<div class='link'>#{link}</div>"
     end.join
+  end
+
+  def term_name
+    today = Date.today
+    term = 2 < today.month || today.month < 9 ? "s" : "f"
+    return "#{today.year}#{term}"
   end
 end
