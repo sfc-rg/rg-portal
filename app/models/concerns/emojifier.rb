a43691b7df11ca090825bed7fcf2532a12dc53a5
Atsuki Demizu <demmys.atsuki@gmail.com>
2015-09-14 22:55:36 +0900

    Avoid deeply nested returning

--- app/models/concerns/emojifier.rb
+++ app/models/concerns/emojifier.rb
@@ -2,13 +2,11 @@ module Emojifier
   def emojify
     self.content.gsub(/:([\w+-]+):/) do |match|
       emoji = Emoji.find_by_alias(Regexp.last_match(1))
-      if emoji
-        return ActionController::Base.helpers.image_tag(
-          ActionController::Base.helpers.image_path("emoji/#{emoji.image_filename}"),
-          alt: match, class: 'emoji'
-        )
-      end
-      match[1]
+      return match[1] unless emoji
+      ActionController::Base.helpers.image_tag(
+        ActionController::Base.helpers.image_path("emoji/#{emoji.image_filename}"),
+        alt: match, class: 'emoji'
+      )
     end
   end
 end
