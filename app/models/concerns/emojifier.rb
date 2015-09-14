1e83ebee4d80fbd8e4eba784ce571ed95b1c1999
Atsuki Demizu <demmys.atsuki@gmail.com>
2015-09-14 11:33:24 +0900

    Fix coding style around regex

--- app/models/concerns/emojifier.rb
+++ app/models/concerns/emojifier.rb
@@ -1,13 +1,14 @@
 module Emojifier
   def emojify
     self.content.gsub(/:([\w+-]+):/) do |match|
-      if emoji = Emoji.find_by_alias($1)
+      emoji = Emoji.find_by_alias(Regexp.last_match(1))
+      if emoji
         ActionController::Base.helpers.image_tag(
           ActionController::Base.helpers.image_path("emoji/#{emoji.image_filename}"),
           alt: match, class: 'emoji'
         )
       else
-        $1
+        match[1]
       end
     end
   end
