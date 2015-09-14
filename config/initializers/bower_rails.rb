0be1251ca0fe3d026883221b157ba8e68d4c51e9
Atsuki Demizu <demmys.atsuki@gmail.com>
2015-09-14 11:11:33 +0900

    add bower settings to run install before precompile

--- config/initializers/bower_rails.rb
+++ config/initializers/bower_rails.rb
@@ -3,13 +3,13 @@ BowerRails.configure do |bower_rails|
   # bower_rails.root_path = Dir.pwd
 
   # Invokes rake bower:install before precompilation. Defaults to false
-  # bower_rails.install_before_precompile = true
+  bower_rails.install_before_precompile = true
 
   # Invokes rake bower:resolve before precompilation. Defaults to false
   # bower_rails.resolve_before_precompile = true
 
   # Invokes rake bower:clean before precompilation. Defaults to false
-  # bower_rails.clean_before_precompile = true
+  bower_rails.clean_before_precompile = true
 
   # Invokes rake bower:install:deployment instead rake bower:install. Defaults to false
   # bower_rails.use_bower_install_deployment = true
