069bace17dd216f95d01711a0e5b4bf561cfc86c
Atsuki Demizu <demmys.atsuki@gmail.com>
2015-09-14 11:37:10 +0900

    reduce line

--- app/models/concerns/emojifier.rb
+++ app/models/concerns/emojifier.rb
@@ -3,13 +3,12 @@ module Emojifier
     self.content.gsub(/:([\w+-]+):/) do |match|
       emoji = Emoji.find_by_alias(Regexp.last_match(1))
       if emoji
-        ActionController::Base.helpers.image_tag(
+        return ActionController::Base.helpers.image_tag(
           ActionController::Base.helpers.image_path("emoji/#{emoji.image_filename}"),
           alt: match, class: 'emoji'
         )
-      else
-        match[1]
       end
+      match[1]
     end
   end
 end
