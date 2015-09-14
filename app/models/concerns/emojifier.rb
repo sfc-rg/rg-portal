197f7fe784083c75791f22d184061300fed0bbba
Atsuki Demizu <demmys.atsuki@gmail.com>
2015-09-14 11:14:12 +0900

    use single quotation in default

--- app/models/concerns/emojifier.rb
+++ app/models/concerns/emojifier.rb
@@ -4,7 +4,7 @@ module Emojifier
       if emoji = Emoji.find_by_alias($1)
         ActionController::Base.helpers.image_tag(
           ActionController::Base.helpers.image_path("emoji/#{emoji.image_filename}"),
-          alt: match, class: "emoji"
+          alt: match, class: 'emoji'
         )
       else
         $1
