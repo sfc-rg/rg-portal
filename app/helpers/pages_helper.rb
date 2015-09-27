48c2b87ee7a337b5ba3a8f1798fe04f7f33afeba
Yusei Yamanaka <yusei1128@gmail.com>
2015-09-27 15:17:55 +0900

    Show subpages list

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,8 +1,7 @@
 module PagesHelper
   def page_breadcrumb_links(page)
-    paths = page.path.split('/')
-    links = paths.map.with_index do |path, index|
-      link = link_to(path, page_path(paths.take(index + 1).join('/')))
+    page.paths.map.with_index do |path, index|
+      link = link_to(path, page_path(page.paths.take(index + 1).join('/')))
       "<div class='link'>#{link}</div>"
     end.join
   end
