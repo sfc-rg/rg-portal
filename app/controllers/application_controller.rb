b47b7c841836d77b8b43f94f83c85d4b3f48c89a
Yusei Yamanaka <yusei1128@gmail.com>
2015-09-28 22:27:02 +0900

    Add before action to set locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   before_action :set_current_user
 
   protect_from_forgery with: :exception
 
   private
+
+  def set_locale
+    I18n.locale = http_accept_language.compatible_language_from(I18n.available_locales)
+  end
 
   def set_current_user
     @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
