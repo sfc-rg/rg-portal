067615d11c5e725af0218b9679dce8c778b73bd3
Yusei Yamanaka <yusei1128@gmail.com>
2015-09-29 10:10:56 +0900

    Add example spec

--- spec/models/page_history_spec.rb
+++ spec/models/page_history_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe PageHistory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:page_history) { FactoryGirl.create(:page_history) }
 end
