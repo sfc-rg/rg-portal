16b946493271d3f2ac943b2eae6ee612e5d31a1b
Yusei Yamanaka <yusei1128@gmail.com>
2015-10-02 09:56:15 +0900

    Syntax Highlighting

--- app/models/concerns/markdown_render.rb
+++ app/models/concerns/markdown_render.rb
@@ -1,9 +1,16 @@
 module MarkdownRender
-  MARKDOWN_RENDER_OPTIONS = { tables: true, autolink: true }
+  MARKDOWN_RENDER_OPTIONS = { tables: true, autolink: true, fenced_code_blocks: true }
   HTML_RENDER_OPTIONS = { hard_wrap: true }
 
   def render_content
     content = self.class.include?(Emojifier) ? emojify : self.content
-    Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(HTML_RENDER_OPTIONS), MARKDOWN_RENDER_OPTIONS).render(content)
+    Redcarpet::Markdown.new(CustomizeRender.new(HTML_RENDER_OPTIONS), MARKDOWN_RENDER_OPTIONS).render(content)
+  end
+
+  class CustomizeRender < Redcarpet::Render::HTML
+    def block_code(code, language)
+      code.gsub!(/\r?\n/, '&#x000A;')
+      "<div><pre><code class=\"language-#{language}\">#{code}</code></pre></div>"
+    end
   end
 end
