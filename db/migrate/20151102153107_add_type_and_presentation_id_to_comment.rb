db59cbb62ba7c6732c711e7028a0e09e40200f0f
Atsuki Demizu <demmys.atsuki@gmail.com>
2015-11-03 15:07:36 +0900

    add presentation_id after page_id

--- db/migrate/20151102153107_add_type_and_presentation_id_to_comment.rb
+++ db/migrate/20151102153107_add_type_and_presentation_id_to_comment.rb
@@ -1,6 +1,7 @@
 class AddTypeAndPresentationIdToComment < ActiveRecord::Migration
   def change
     add_column :comments, :type, :string
-    add_reference :comments, :presentation, index: true
+    add_column :comments, :presentation_id, :integer, after: :page_id
+    add_index :comments, :presentation_id
   end
 end
