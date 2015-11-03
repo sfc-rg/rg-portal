2fc77f6ad1380912d5679f32873218a5aca5fb1f
Atsuki Demizu <demmys.atsuki@gmail.com>
2015-11-03 17:52:37 +0900

    Add MeetingsController specs

--- spec/controllers/meetings_controller_spec.rb
+++ spec/controllers/meetings_controller_spec.rb
@@ -2,15 +2,36 @@ require 'rails_helper'
 
 RSpec.describe MeetingsController, type: :controller do
   render_views
+  let(:meeting) { FactoryGirl.create(:meeting) }
   before { login_as_user }
 
   describe '#index' do
-    before do
-      get :index
-    end
+    before { get :index }
+    it { is_expected.to render_template(:index) }
+  end
 
-    it 'renders index action' do
-      expect(response).to render_template(:index)
-    end
+  describe '#show' do
+    subject { get :show, id: meeting.id }
+    it { is_expected.to render_template :show }
+  end
+
+  describe '#new' do
+    subject { get :new }
+    it { is_expected.to render_template :new }
+  end
+
+  describe '#create' do
+    subject { post :create, meeting: FactoryGirl.attributes_for(:meeting) }
+    it { is_expected.to redirect_to meetings_path }
+  end
+
+  describe '#edit' do
+    subject { get :edit, id: meeting.id }
+    it { is_expected.to render_template :edit }
+  end
+
+  describe '#update' do
+    subject { patch :update, id: meeting.id, meeting: FactoryGirl.attributes_for(:meeting) }
+    it { is_expected.to redirect_to meeting_path(meeting) }
   end
 end
--- spec/factories/meetings.rb
+++ spec/factories/meetings.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :meeting do
     name '講義1'
+    content 'This is a meeting'
     start_at 1.hour.ago
     end_at 1.hour.since
   end
