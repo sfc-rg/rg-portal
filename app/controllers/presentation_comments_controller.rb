06b2f9b2faaf9faa600b370beb28c80a36c7eb1b
Yusei Yamanaka <yusei1128@gmail.com>
2015-12-02 23:12:22 +0900

    Add slack notification on presentation comment

--- app/controllers/presentation_comments_controller.rb
+++ app/controllers/presentation_comments_controller.rb
@@ -1,5 +1,9 @@
 class PresentationCommentsController < CommentsController
+  include SlackNotifier
+
   before_action :set_presentation, only: :index
+  after_action :notify_new_comment, only: :create
+  after_action :notify_mentions, only: :create
 
   def index
     return render json: { html: nil }, status: 404 if @presentation.blank?
@@ -23,5 +27,20 @@ class PresentationCommentsController < CommentsController
 
   def set_presentation
     @presentation = Presentation.find_by(id: params[:presentation_id])
+  end
+
+  def notify_new_comment
+    return if @comment.user == @comment.presentation.user
+    message = "New comment on #{@comment.presentation.title} by #{@comment.user.nickname}\n#{@comment.content}\n#{presentation_url(@comment.presentation)}"
+    slack_notify(from: @comment.user, to: @comment.presentation.user, message: message)
+  end
+
+  def notify_mentions
+    usernames = @comment.content.scan(MENTION_USER_REGEX).map { |mention| mention[0] }
+    mention_users = usernames.map { |username| User.find_by(nickname: username) }.compact
+    mention_users.each do |mention_user|
+      message = "New mention on #{@comment.presentation.title} by #{@comment.user.nickname}\n#{@comment.content}\n#{presentation_url(@comment.presentation)}"
+      slack_notify(from: @comment.user, to: mention_user, message: message)
+    end
   end
 end
