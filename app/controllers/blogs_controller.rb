9d2a1fd5c4285ddc7ecaf7154e88dba1757717fc
Yusei Yamanaka <yusei1128@gmail.com>
2015-12-09 17:58:52 +0900

    Replace Time.zone.local instead of Time.local

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -54,7 +54,7 @@ class BlogsController < ApplicationController
   def set_blog
     date = params[:timestamp]
     timeoptions = [date[0...4], date[4...6], date[6...8], date[8...10], date[10...12], date[12...14], date[14...20]]
-    @blog = Blog.find_by!(user: @user, created_at: Time.local(*timeoptions.map(&:to_i)))
+    @blog = Blog.find_by!(user: @user, created_at: Time.zone.local(*timeoptions.map(&:to_i)))
   end
 
   def blog_params
