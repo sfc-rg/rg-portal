251251c0347ccbceb02dd24314a941261200a201
Yusei Yamanaka <yusei1128@gmail.com>
2016-01-30 08:59:12 +0900

    Optimize slack scopes using object scopes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 OAUTH_CONFIG = HashWithIndifferentAccess.new(YAML.load(File.open(Rails.root.join('config', 'oauth.yml'))))
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :slack, OAUTH_CONFIG[:slack][:client_id], OAUTH_CONFIG[:slack][:client_secret], scope: :client, team: OAUTH_CONFIG[:slack][:team_id]
+  provider :slack, OAUTH_CONFIG[:slack][:client_id], OAUTH_CONFIG[:slack][:client_secret],
+    team: OAUTH_CONFIG[:slack][:team_id], scope: 'channels:read chat:write:bot chat:write:user'
 end
