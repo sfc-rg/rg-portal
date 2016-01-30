55f7031a2f17b72da57fa35b0026a117cef38c07
Yusei Yamanaka <yusei1128@gmail.com>
2016-01-30 11:44:35 +0900

    Add object scopes used by omniauth-slack

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OAUTH_CONFIG = HashWithIndifferentAccess.new(YAML.load(File.open(Rails.root.join
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :slack, OAUTH_CONFIG[:slack][:client_id], OAUTH_CONFIG[:slack][:client_secret],
-    team: OAUTH_CONFIG[:slack][:team_id], scope: 'channels:read chat:write:bot chat:write:user'
+    team: OAUTH_CONFIG[:slack][:team_id], scope: 'channels:read chat:write:bot chat:write:user users:read team:read'
 end
