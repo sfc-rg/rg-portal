9f56ddced2fbb509de7cea086f351ae6754a893a
Yusei Yamanaka <yusei1128@gmail.com>
2016-10-04 17:17:37 +0900

    Add api keys management controller

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -3,7 +3,19 @@ class ApiKey < ActiveRecord::Base
 
   belongs_to :user
 
+  validates :access_token, presence: true, uniqueness: true
+  validates :user, presence: true
+
   def generate_access_token!
     self.access_token = SecureRandom.hex(ACCESS_TOKEN_LENGTH/2)
+  end
+
+  def revoked?
+    self.revoked_at.present?
+  end
+
+  def revoke!
+    self.revoked_at = Time.now
+    self.save!(validate: false)
   end
 end
