0a796794c23090bd6ccb0be917f7e25c013eded5
Yusei Yamanaka <yusei1128@gmail.com>
2016-10-04 12:00:37 +0900

    Show user icon with nickname

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,11 @@ module ApplicationHelper
   def displayable_datetime(time)
     time.strftime('%Y/%m/%d %H:%M:%S')
   end
+
+  def show_user_badge(user)
+    content_tag(:div, class: 'user_badge') do
+      concat image_tag(user.icon_url, class: 'user_icon')
+      concat content_tag(:span, user.nickname, class: 'user_nickname')
+    end
+  end
 end
