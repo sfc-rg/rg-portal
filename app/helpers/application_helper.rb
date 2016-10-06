16e6971c78bf0592715caf216768575fa7571503
Yusei Yamanaka <yusei1128@gmail.com>
2016-10-06 09:10:50 +0900

    Fix some designs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,7 @@ module ApplicationHelper
 
   def show_user_badge(user)
     return if user.blank?
-    content_tag(:div, class: 'user_badge') do
+    content_tag(:span, class: 'user_badge') do
       concat image_tag(user.icon_url, class: 'user_icon')
       concat content_tag(:span, user.nickname, class: 'user_nickname')
     end
